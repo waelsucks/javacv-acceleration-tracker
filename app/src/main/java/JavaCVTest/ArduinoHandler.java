55d13452f36a0a5440b8b3bd509faf3074c46be1
kitcanna <89905869+kitcanna@users.noreply.github.com>
2022-04-20 13:22:22 +0200

    Update ArduinoHandler.java

---
+++ app/src/main/java/JavaCVTest/ArduinoHandler.java
@@ -0,0 +1,63 @@
+package JavaCVTest;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ArduinoHandler {
+    private int port = 1337;
+    private String ipArd = "192.168.0.12";
+    private DataOutputStream dos;
+    private DataInputStream dis;
+    private byte messageFromArduino;
+    private String ledOn = "a";
+
+    public ArduinoHandler() throws IOException {
+        connectToArduino();
+    }
+
+    public void connectToArduino() throws IOException {
+
+            Socket socket = new Socket(ipArd, port);
+            System.out.println("new game started");
+            dos = new DataOutputStream(socket.getOutputStream());
+            dis = new DataInputStream(socket.getInputStream());
+            new ArduinoHandler();
+
+    }
+
+    public void run(){ // java serverns tråd
+    }
+
+    private class Handler extends Thread{
+
+        public Handler(){
+            start();
+        }
+
+        @Override
+        public void run() {
+            try {
+                while(!interrupted()) {
+                    messageFromArduino = dis.readByte();
+                    System.out.println("arduino-server called victory");
+
+                    if(messageFromArduino == 71){
+                        System.out.println("YOU WON!");
+                        dos.write(ledOn.getBytes());
+                        dos.flush();
+                        currentThread().interrupt();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new ArduinoHandler();
+
+    }
+
+}
